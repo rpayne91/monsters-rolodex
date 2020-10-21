fe2a47b3b80cb19e9caf8876b061c94ace0a3202
Rory <rorygp@gmail.com>
2020-10-21 10:02:02 +0200

    Updates

---
+++ precache-manifest.91519d07b43f1a62dffa036b18c289ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91b288f1ae60dfe60950d0d20279e8e4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "71d3af09114d3026a8f3",
+    "url": "/monsters-rolodex/static/css/main.a1d5d5b2.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71d3af09114d3026a8f3",
+    "url": "/monsters-rolodex/static/js/main.701892c9.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
